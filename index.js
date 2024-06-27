891457cc2385ef9c4f5144084580567f90b4d7fb
alvindrawan8 <alviindra@gmail.com>
2024-06-27 13:18:30 +0700

    add 4 console

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// console.log("hello, git!")
-console.log("hoho");
-console.log("sontoloyo");
-console.log("ada apa");
+console.log("hello, git!")
+console.log("hello, git!")
+console.log("hello, git!")
+console.log("hello, git!")
 
+console.log("hihi") 
